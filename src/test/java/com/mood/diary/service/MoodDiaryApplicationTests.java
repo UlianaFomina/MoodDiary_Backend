10b61b65e37c1aa48d04d8b3af30f55405430d0b
shimawe <Eugene.Vozny@ipsotek.com>
2023-05-06 10:40:23 +0200

    init: init structure

---
+++ src/test/java/com/mood/diary/service/MoodDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mood.diary.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
